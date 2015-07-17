461d11789d3e7bdd78aabc17d02252a734505e2e
bjoerntx <bjoern@textcontrol.com>
2015-07-17 18:15:01 +0200

    Uploaded sample

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace tx_watermark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.watermarkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.ButtonBar = this.buttonBar1;
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 77);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.RulerBar = this.rulerBar2;
+            this.textControl1.Size = new System.Drawing.Size(895, 626);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 0;
+            this.textControl1.VerticalRulerBar = this.rulerBar1;
+            this.textControl1.Changed += new System.EventHandler(this.textControl1_Changed);
+            this.textControl1.ImageSelected += new TXTextControl.ImageEventHandler(this.textControl1_ImageSelected);
+            this.textControl1.ImageDeselected += new TXTextControl.ImageEventHandler(this.textControl1_ImageDeselected);
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(920, 28);
+            this.buttonBar1.TabIndex = 1;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 703);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(920, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 77);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 626);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 52);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(920, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.watermarkToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(920, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // watermarkToolStripMenuItem
+            // 
+            this.watermarkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertToolStripMenuItem});
+            this.watermarkToolStripMenuItem.Name = "watermarkToolStripMenuItem";
+            this.watermarkToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.watermarkToolStripMenuItem.Text = "Watermark";
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.insertToolStripMenuItem.Text = "Insert";
+            this.insertToolStripMenuItem.Click += new System.EventHandler(this.insertToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 725);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TX Text Control: Watermark Images";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.RulerBar rulerBar2;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem watermarkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tx_watermark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int iNumberOfPages = 1;
+
+        private void insertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InsertWatermarkImages();
+        }
+
+        private void InsertWatermarkImages()
+        {
+            // remove all existing watermarks
+            RemoveWatermarkImages(textControl1);
+
+            foreach (TXTextControl.Page page in textControl1.GetPages())
+            {
+                // create a new watermark image
+                Bitmap bmp = CreateDraftImage();
+
+                // create a new TXTextControl.Image object and mark
+                // as watermark
+                TXTextControl.Image img = new TXTextControl.Image(bmp);
+                img.Name = "Watermark";
+                img.Sizeable = false;
+                img.Moveable = false;
+
+                // calculate the location to center the image
+                Point pImageLocation = new Point(
+                    (page.Bounds.Width / 2) - (PixelToTwips(bmp.Size.Width)) / 2,
+                    (page.Bounds.Height / 2) - (PixelToTwips(bmp.Size.Height)) / 2);
+
+                // add the image to the page
+                textControl1.Images.Add(
+                    img,
+                    page.Number,
+                    pImageLocation,
+                    TXTextControl.ImageInsertionMode.BelowTheText);
+            }
+        }
+
+        // creates a new "DRAFT" sample image
+        private Bitmap CreateDraftImage()
+        {
+            string sText = "DRAFT";
+
+            Bitmap destination = new Bitmap(400, 400);
+            using (Graphics g = Graphics.FromImage(destination))
+            {
+                GraphicsUnit units = GraphicsUnit.Pixel;
+
+                g.Clear(Color.White);
+                StringFormat stringFormat = new StringFormat();
+                stringFormat.Alignment = StringAlignment.Center;
+                stringFormat.LineAlignment = StringAlignment.Center;
+
+                g.DrawString(sText, new Font(
+                    "Arial", 
+                    90, 
+                    FontStyle.Bold,
+                    GraphicsUnit.Pixel), 
+                    new SolidBrush(
+                        Color.FromArgb(64, Color.Black)),
+                        destination.GetBounds(ref units),
+                        stringFormat);
+            }
+
+            return destination;
+        }
+
+        // converts pixels to twips
+        static int PixelToTwips(int pixel)
+        {
+            float DpiX = (float)(1440 / Graphics.FromHwnd(new IntPtr()).DpiX);
+            return (int)(pixel * DpiX);
+        }
+
+        private void RemoveWatermarkImages(TXTextControl.TextControl textControl)
+        {
+            // List of images to be deleted
+            List<TXTextControl.Image> lImagesToRemove = 
+                new List<TXTextControl.Image>();
+
+            // loop through all images and check for the Name
+            foreach (TXTextControl.Image img in textControl1.Images)
+            {
+                // add watermark images to the List
+                if (img.Name == "Watermark")
+                    lImagesToRemove.Add(img);
+            }
+
+            // remove all tagged watermark images
+            foreach(TXTextControl.Image img in lImagesToRemove)
+            {
+                textControl1.Images.Remove(img);
+            }
+        }
+
+        // set the EditMode to read only when a watermark is selected
+        // to avoid the deletion
+        private void textControl1_ImageSelected(object sender, TXTextControl.ImageEventArgs e)
+        {
+            if (e.Image.Name == "Watermark")
+                textControl1.EditMode = TXTextControl.EditMode.ReadAndSelect;
+        }
+
+        // set back the EditMode 
+        private void textControl1_ImageDeselected(object sender, TXTextControl.ImageEventArgs e)
+        {
+            textControl1.EditMode = TXTextControl.EditMode.Edit;
+        }
+
+        // update the watermarks if the number of pages are different
+        private void textControl1_Changed(object sender, EventArgs e)
+        {
+            if (textControl1.Pages > iNumberOfPages)
+            {
+                InsertWatermarkImages();
+                iNumberOfPages = textControl1.Pages;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InsertWatermarkImages();
+        }
+    }
+}
